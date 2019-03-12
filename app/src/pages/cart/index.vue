dc451209bff0c8c9c35c0fd6f457d9fe65429fa1
areschen <ariesjia00@hotmail.com>
2019-03-12 11:26:38 +0800

    init

--- app/src/pages/cart/index.vue
+++ app/src/pages/cart/index.vue
@@ -9,22 +9,15 @@
         @amount-change="handleAmountChange(index, arguments)"
       />
     </div>
-    <div class="bottom-action">
-      <button class="btn" @click="toggleCart">添加商品</button>
-    </div>
-    <div class="popup" v-show="shouldShowCart">
-      <div class="backdrop" @click="toggleCart"></div>
-      <form class="popup-content" @submit.prevent="confirm">
-        <h3>添加商品</h3>
-        <input type="text" v-model="code" autofocus placeholder="请输入商品编号">
-        <button class="btn" type="submit">确定</button>
-      </form>
-    </div>
+    <add-product
+        @add="handleAddProduct"
+    />
   </div>
 </template>
 
 <script>
   import item from './components/cartItem'
+  import addProduct from './components/addProduct'
 
   const generateProduct = code => ({
     code,
@@ -35,6 +28,7 @@
     name: 'cart',
     components: {
       item,
+      addProduct,
     },
     data: function() {
       return {
@@ -51,95 +45,20 @@
       },
     },
     methods: {
-      toggleCart: function() {
-        this.shouldShowCart = !this.shouldShowCart
-      },
       handleAmountChange(index, agrs) {
         const amount = agrs[0]
         this.cartProducts[index].amount = amount
       },
-      confirm: function($event) {
-        if (this.code) {
-          const index = this.cartProducts.findIndex(product => product.code === this.code)
-          if (index >= 0) {
-            this.cartProducts[index].amount += 1
-          } else {
-            this.cartProducts.push(generateProduct(this.code))
-          }
-          this.code = ''
-          this.shouldShowCart = false
+      handleAddProduct: function(code) {
+        const index = this.cartProducts.findIndex(product => product.code === code)
+        if (index >= 0) {
+          this.cartProducts[index].amount += 1
+        } else {
+          this.cartProducts.push(generateProduct(code))
         }
-        $event.preventDefault()
       },
     },
   }
 </script>
 
-<style>
-  .title {
-    padding: 5px 10px;
-    background-color: #000;
-    text-align: right;
-    color: #fff;
-  }
-
-  .list {
-    margin: 10px 10px 50px;
-  }
-
-  .product {
-    border-bottom: 1px dashed #000;
-    margin-bottom: 5px;
-    padding: 5px;
-    line-height: 1.6;
-    display: flex;
-    font-size: 12px;
-  }
-
-  .product .code {
-    flex: 1;
-  }
-
-  .product .actions button {
-    background: #000;
-    color: #fff;
-    border: 0 none;
-    padding: 3px 5px;
-    vertical-align: middle;
-  }
-
-  .bottom-action {
-    position: fixed;
-    bottom: 10px;
-    left: 10px;
-    right: 10px;
-  }
-
-  .popup {
-  }
-
-  .popup .popup-content {
-    position: fixed;
-    background-color: #fff;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    padding: 10px;
-    z-index: 10;
-    animation: fadeUp .4s ease both;
-  }
-
-  .popup .popup-content input {
-    margin: 10px 0;
-  }
-
-  .backdrop {
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 2;
-    background-color: rgba(0, 0, 0, .6);
-  }
-</style>
+<style src="./cart.css" />
