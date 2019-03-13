42adf746d551f32caa8d6e1a80ca55c42ea7d227
Jimmy Lv <jimmy.jinglv@gmail.com>
2019-03-13 19:03:57 +0800

    added /cart

--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import cart from './pages/cart'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'cart',
+      component: cart
     },
     {
       path: '/about',
@@ -17,7 +17,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: function () { 
+      component: function () {
         return import(/* webpackChunkName: "about" */ './views/About.vue')
       }
     }
