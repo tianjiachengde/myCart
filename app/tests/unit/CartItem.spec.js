2e0df71ccc2401c3d76d19705c2028a558b20a7f
Jimmy Lv <jimmy.jinglv@gmail.com>
2019-03-13 18:34:11 +0800

    added unit test for cart item

---
+++ app/tests/unit/CartItem.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils'
+
+import CartItem from '../../src/pages/cart/components/cartItem'
+
+describe('CartItem component', () => {
+
+  it('should emit event when click minus button', () => {
+    // given
+    const product = { code: 'one', price: 100, amount: 2 }
+
+    // when
+    const wrapper = shallowMount(CartItem, { propsData: { product }})
+    wrapper.find('button').trigger('click')
+
+    // then
+    expect(wrapper.emitted('amount-change')).toBeTruthy()
+  })
+
+  it('should not emit event when amount is below 1 and click minus button', () => {
+    // given
+    const product = { code: 'one', price: 100, amount: 1 }
+
+    // when
+    const wrapper = shallowMount(CartItem, { propsData: { product }})
+    wrapper.find('button').trigger('click')
+
+    // then
+    expect(wrapper.emitted('amount-change')).not.toBeTruthy()
+  })
+
+})
