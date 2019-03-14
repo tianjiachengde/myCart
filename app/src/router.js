1f09d2f7458dda2dc0bf83f87d7b16659911e9fb
jiacheng <jiacheng.tian@baozun.com>
2019-03-14 18:20:36 +0800

    Revert "update history mode and routes"
    This reverts commit e9d5f1341aa65b14c7b4d442c7aff3e7acb3460e.

--- app/src/router.js
+++ app/src/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import cart from './views/cart'
 import detail from './views/detail'
@@ -9,12 +11,11 @@ import NotFound from './views/404'
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
   routes: [
     { path: '/', redirect: '/cart' },
     { path: '/cart', name: 'cart', component: cart },
     { path: '/products/:id', name: 'detail', component: detail },
-    { path: '/counter', name: 'counter', component: counter },
+    { path: '/counter', name: 'detail', component: counter },
     { path: '*', name: 'NotFound', component: NotFound },
     {
       path: '/about',
@@ -24,10 +25,24 @@ const router = new Router({
       // which is lazy-loaded when the route is visited.
       component: function () {
         return import(/* webpackChunkName: "about" */ './views/About.vue')
-      },
-    },
-  ],
+      }
+    }
+  ]
 })
 
-export const createRouter = () => router
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    NProgress.start()
+  }
+  next()
+})
+
+router.afterEach((to, from) => {
+
+  // Complete the animation of the route progress bar.
+  NProgress.done()
+})
+
 export default router
